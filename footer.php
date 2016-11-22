b28bce7b02fb64b76908efbb876dee876a1d17be
Jeff Fabiny <jefffabiny@gmail.com>
2016-11-22 17:52:19 -0500

    The foundation. Should work if you zip it up and install it into wordpress.

---
+++ footer.php
@@ -0,0 +1,11 @@
+    </div> <!-- /.container -->
+
+		<footer class="theme-footer">
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
+
+<?php wp_footer(); ?> 
+  </body>
+</html>
